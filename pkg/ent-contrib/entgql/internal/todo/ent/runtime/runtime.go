9b8204dc35dfedc0f88f2a0f04817826e42ff859
Alex Snast <alexsn@fb.com>
2020-07-19 09:00:23 -0700

    symphony/ent-contrib: rename entgqlgen to entgql
    Summary: Prefer shorter names.
    
    Reviewed By: naor9991
    
    Differential Revision: D22621067
    
    fbshipit-source-id: f4d3f2e60b86db1f1ede22c9043c4b740d00ba3a

---
+++ pkg/ent-contrib/entgql/internal/todo/ent/runtime/runtime.go
@@ -0,0 +1,14 @@
+// Copyright (c) 2004-present Facebook All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Code generated (@generated) by entc, DO NOT EDIT.
+
+package runtime
+
+// The schema-stitching logic is generated in github.com/facebookincubator/symphony/pkg/ent-contrib/entgql/internal/todo/ent/runtime.go
+
+const (
+	Version = "v0.2.8-0.20200719150104-54f0a6769bda"            // Version of ent codegen.
+	Sum     = "h1:tqhV1WAdf0ipoXYcslPaHH53OkiIVvcKz8kdq6GdmlE=" // Sum of ent codegen.
+)
