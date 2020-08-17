45ef398193046dedc20cdd4230ad46eca39095ed
Alex Snast <alexsn@fb.com>
2020-08-17 19:59:21 +0300

    remove network hub code
    Signed-off-by: Alex Snast <alexsn@fb.com>

--- app/fbcnms-projects/platform-server/src/main/routes.js
+++ app/fbcnms-projects/platform-server/src/main/routes.js
@@ -97,7 +97,6 @@ router.get(
 router.get('/admin*', access(AccessRoles.SUPERUSER), handleReact('admin'));
 router.get('/automation*', access(AccessRoles.USER), handleReact('automation'));
 router.get('/inventory*', access(AccessRoles.USER), handleReact('inventory'));
-router.get('/hub*', access(AccessRoles.USER), handleReact('inventory'));
 router.get('/workorders*', access(AccessRoles.USER), handleReact('workorders'));
 router.get('/id*', access(AccessRoles.USER), handleReact('id'));
 router.use(
