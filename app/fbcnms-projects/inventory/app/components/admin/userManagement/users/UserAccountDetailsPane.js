bdaaf6f79faa358a616b64775da5c1be9e15fbf5
Amit Arbel <arbelamit@fb.com>
2020-10-05 10:28:46 +0300

    EZ - Fixing button tag related JSS rule using data attribute instead of tag (#1520)

--- app/fbcnms-projects/inventory/app/components/admin/userManagement/users/UserAccountDetailsPane.js
+++ app/fbcnms-projects/inventory/app/components/admin/userManagement/users/UserAccountDetailsPane.js
@@ -37,7 +37,7 @@ const useStyles = makeStyles(() => ({
   },
   actionButtons: {
     marginTop: '24px',
-    '& > button': {
+    '& > [data-is-clickable]': {
       marginRight: '8px',
     },
   },
