c157a6b2dcdc9c0f2ebe3d9ccf50ebe06cc4d963
Jorge <dalmendray@fb.com>
2020-11-18 10:51:20 -0800

    Mobile (#1684)
    * Adding symphony-mobile into symphony
    
    * Adding symphony-mobile to symphony
    
    * Remove .DS_STORE
    
    * Excluding mobile from lint/flow

---
+++ app/mobile/android/app/build/generated/source/buildConfig/platform/debug/com/fbc/BuildConfig.java
@@ -0,0 +1,16 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.fbc;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "cloud.thesymphony";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "platform";
+  public static final int VERSION_CODE = 33;
+  public static final String VERSION_NAME = "debug 2020.11.18.10:04:52";
+  // Fields from default config.
+  public static final String APP_NAME = "platform";
+  public static final String MAPBOX_TOKEN = "pk.eyJ1IjoiZmJtYXBzIiwiYSI6ImNqOGFmamkxdTBmbzUyd28xY3lybnEwamIifQ.oabgbuGc81ENlOJoPhv4OQ";
+}
