8814a9da50cac09268ae9a349f5a3bb3b83d798c
mescorci <74663735+mescorci@users.noreply.github.com>
2021-01-16 09:53:29 -0500

    Update utils.go

--- pkg/exporter/utils.go
+++ pkg/exporter/utils.go
@@ -18,7 +18,7 @@ import (
 	"github.com/facebookincubator/symphony/pkg/ent/equipmentport"
 	"github.com/facebookincubator/symphony/pkg/ent/link"
 	"github.com/facebookincubator/symphony/pkg/ent/location"
-	"github.com/facebookincubator/symphony/pkg/ent/project"													
+	"github.com/facebookincubator/symphony/pkg/ent/project"
 	"github.com/facebookincubator/symphony/pkg/ent/property"
 	"github.com/facebookincubator/symphony/pkg/ent/propertytype"
 	"github.com/facebookincubator/symphony/pkg/ent/schema/enum"
