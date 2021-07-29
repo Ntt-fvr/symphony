142e97ef0d84a91f68f3f413d7a74fd60af7c707
Sergio Hortua <sergioandresjsv@gmail.com>
2021-07-28 19:23:06 -0500

    Fix - Styles Form create Kqi

--- app/fbcnms-projects/inventory/app/components/assurance/KqiFormCreate.js
+++ app/fbcnms-projects/inventory/app/components/assurance/KqiFormCreate.js
@@ -8,16 +8,20 @@
  * @format
  */
 
-import React, {useState} from 'react';
+import React from 'react';
 import fbt from 'fbt';
 
 import TextInput from '@symphony/design-system/components/Input/TextInput';
+import classNames from 'classnames';
 
 import Button from '@material-ui/core/Button';
 import Card from '@symphony/design-system/components/Card/Card';
 import FormField from '@symphony/design-system/components/FormField/FormField';
 import Grid from '@material-ui/core/Grid';
+import {MenuItem, Select} from '@material-ui/core';
+
 import Text from '@symphony/design-system/components/Text';
+
 import TextField from '@material-ui/core/TextField';
 import {StatusActive} from './AlarmFilteringStatus';
 
@@ -30,8 +34,30 @@ const useStyles = makeStyles(() => ({
     flexGrow: 1,
     margin: '40px',
   },
+  select: {
+    '& .MuiSelect-select': {
+      padding: '9px 0 0 10px',
+    },
+    border: '1px solid #D2DAE7',
+    height: '36px',
+    overflow: 'hidden',
+    position: 'relative',
+    boxSizing: 'border-box',
+    minHeight: '36px',
+    borderRadius: '4px',
+    fontSize: '14px',
+  },
+  selectRepeatEvery: {
+    width: '67%',
+    marginLeft: '1rem',
+  },
   formField: {
-    margin: '0 43px 22px 0',
+    margin: '0 1rem 1rem 1rem',
+  },
+  formFieldTf: {
+    width: '24rem',
+    height: 'auto',
+    margin: '0 1rem 1rem 0',
   },
   formFieldStatus: {
     marginTop: '1rem',
@@ -52,11 +78,7 @@ const useStyles = makeStyles(() => ({
     marginLeft: '10px',
   },
   textTitle: {
-    paddingLeft: '2rem',
-  },
-  titleButtons: {
-    padding: '1rem 1rem 0 1rem',
-    alignItems: 'center',
+    paddingLeft: '3rem',
   },
   reason: {
     minHeight: '100px',
@@ -64,82 +86,128 @@ const useStyles = makeStyles(() => ({
   status: {
     paddingTop: '40px',
   },
-  time: {
-    marginBottom: '20px',
+  time: {},
+  titleTime: {
+    marginLeft: '1rem',
   },
 }));
-
+const data = {
+  counters: {
+    edges: [
+      {
+        node: {
+          id: '244813135872',
+          name: 'contador_family_7',
+          networkManagerSystem: 'hola bebe',
+          externalID: '123456789',
+        },
+      },
+      {
+        node: {
+          id: '244813135873',
+          name: 'contador_family_8',
+          networkManagerSystem: 'hola sergio',
+          externalID: '987654321',
+        },
+      },
+    ],
+  },
+};
 const KqiFormCreate = props => {
   const classes = useStyles();
 
   return (
     <div className={classes.root}>
-      <Grid container spacing={3}>
-        <Grid container className={classes.titleButtons}>
-          <Grid item xs={10}>
-            <Text className={classes.textTitle} variant="h6">
-              {fbt('Create KQI', ' ')}
-            </Text>
-          </Grid>
-          <Grid item xs={2}>
-            <Grid container>
-              <Grid xs={6}>
-                <FormField>
-                  <Button
-                    className={classes.option}
-                    variant="outlined"
-                    color="primary"
-                    onClick={props.returnTableKqi}>
-                    Cancel
-                  </Button>
-                </FormField>
-              </Grid>
-              <Grid xs={6}>
-                <FormField>
-                  <Button
-                    onClick={props.returnTableKqi}
-                    className={classes.option}
-                    variant="contained"
-                    color="primary">
-                    Save
-                  </Button>
-                </FormField>
-              </Grid>
+      <Grid container spacing={1}>
+        <Grid item xs={10}>
+          <Text className={classes.textTitle} variant="h6">
+            {fbt('Create KQI', ' ')}
+          </Text>
+        </Grid>
+        <Grid item xs={2}>
+          <Grid container>
+            <Grid xs={6}>
+              <FormField>
+                <Button
+                  className={classes.option}
+                  variant="outlined"
+                  color="primary"
+                  onClick={props.returnTableKqi}>
+                  Cancel
+                </Button>
+              </FormField>
+            </Grid>
+            <Grid xs={6}>
+              <FormField>
+                <Button
+                  onClick={props.returnTableKqi}
+                  className={classes.option}
+                  variant="contained"
+                  color="primary">
+                  Save
+                </Button>
+              </FormField>
             </Grid>
           </Grid>
         </Grid>
+
         <Grid item xs={12}>
           <Card>
-            <Grid container>
-              <Grid item xs={1}>
-                <FormField label="Enabled">
-                  <Switch />
-                </FormField>
-              </Grid>
-              <Grid item xs={11}>
-                <FormField className={classes.formField} label="Name">
+            <Grid container spacing={1}>
+              <Grid item xs={6}>
+                <FormField label="Name" className={classes.formField}>
                   <TextInput className={classes.textInput} />
                 </FormField>
               </Grid>
               <Grid item xs={6}>
-                <FormField
-                  label="Network Resource"
-                  className={classes.formField}>
+                <FormField className={classes.formField} label="ID">
                   <TextInput className={classes.textInput} />
                 </FormField>
               </Grid>
+              <Grid container item xs={6}>
+                <Grid item xs={6}>
+                  <FormField label="Category" className={classes.formField}>
+                    <Select
+                      className={classes.select}
+                      disableUnderline
+                      name="family">
+                      {data.counters.edges.map((item, index) => (
+                        <MenuItem key={index} value={item.node?.id}>
+                          {item.node?.name}
+                        </MenuItem>
+                      ))}
+                    </Select>
+                  </FormField>
+                </Grid>
+                <Grid item xs={6}>
+                  <FormField label="Perspective" className={classes.formField}>
+                    <Select
+                      className={classes.select}
+                      disableUnderline
+                      name="family">
+                      {data.counters.edges.map((item, index) => (
+                        <MenuItem key={index} value={item.node?.id}>
+                          {item.node?.name}
+                        </MenuItem>
+                      ))}
+                    </Select>
+                  </FormField>
+                </Grid>
+              </Grid>
               <Grid item xs={6}>
-                <FormField className={classes.formField} label="Reason">
+                <FormField className={classes.formField} label="Description">
                   <TextInput
                     className={classes.textInput}
                     type="multiline"
-                    rows={4}
+                    rows={3}
                   />
                 </FormField>
               </Grid>
               <Grid container item xs={6}>
                 <Grid className={classes.time} item xs={12}>
-                  <Text variant="subtitle1">Exception period</Text>
+                  <Text className={classes.titleTime} variant="subtitle1">
+                    Activation period
+                  </Text>
                 </Grid>
                 <Grid item xs={6}>
                   <FormField label="Start" className={classes.formField}>
@@ -161,18 +229,52 @@ const KqiFormCreate = props => {
                     />
                   </FormField>
                 </Grid>
+                <Grid item xs={6}>
+                  <FormField label="Source" className={classes.formField}>
+                    <Select
+                      className={classes.select}
+                      disableUnderline
+                      name="family">
+                      {data.counters.edges.map((item, index) => (
+                        <MenuItem key={index} value={item.node?.id}>
+                          {item.node?.name}
+                        </MenuItem>
+                      ))}
+                    </Select>
+                  </FormField>
+                </Grid>
+                <Grid className={classes.prueba} container item xs={6}>
+                  <FormField
+                    label="Temporal frequency"
+                    className={classes.formField}>
+                    <div className={classes.formFieldTf}>
+                      <Text>Repeat every</Text>
+
+                      <Select
+                        className={classNames(
+                          classes.select,
+                          classes.selectRepeatEvery,
+                        )}
+                        disableUnderline
+                        name="family">
+                        {data.counters.edges.map((item, index) => (
+                          <MenuItem key={index} value={item.node?.id}>
+                            {item.node?.name}
+                          </MenuItem>
+                        ))}
+                      </Select>
+                    </div>
+                  </FormField>
+                </Grid>
               </Grid>
-              <Grid container item xs={6} className={classes.status}>
-                <Grid item xs={3}>
-                  <FormField label="Status" className={classes.formField}>
-                    <StatusActive className={classes.formFieldStatus} />
-                  </FormField>
-                </Grid>
-                <Grid item xs={9}>
-                  <FormField label="ID" className={classes.formField}>
-                    <TextInput className={classes.textInput} />
-                  </FormField>
-                </Grid>
+              <Grid item xs={6}>
+                <FormField label="Formula" className={classes.formField}>
+                  <TextInput
+                    type="multiline"
+                    rows={10}
+                    className={classes.textInput}
+                  />
+                </FormField>
               </Grid>
             </Grid>
           </Card>
