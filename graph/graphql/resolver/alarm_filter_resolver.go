b3b63338aa5147570f9585842d1a7be7516681f4
mescorci <mescorci@everis.com>
2021-08-27 12:27:47 -0500

    FIX LINT ERRORS 9
    FIX LINT ERRORS 9

--- graph/graphql/resolver/alarm_filter_resolver.go
+++ graph/graphql/resolver/alarm_filter_resolver.go
@@ -60,7 +60,7 @@ func (r mutationResolver) RemoveAlarmFilter(ctx context.Context, id int) (int, e
 	if err != nil {
 		return id, errors.Wrapf(err, "has occurred error on process: %v", err)
 	}
-	//TODO: borrar o editar los edges relacionados
+	// TODO: borrar o editar los edges relacionados
 
 	if err := client.AlarmFilter.DeleteOne(t).Exec(ctx); err != nil {
 		return id, errors.Wrap(err, "has occurred error on process: %v")
