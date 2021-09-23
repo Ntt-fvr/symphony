f740f3943b87c17c8c627341889cd6fc357b0531
mescorci <mescorci@everis.com>
2021-09-23 08:51:42 -0500

    Update __init__.py
    add more time to wait_for_platform

--- cli/tests/utils/__init__.py
+++ cli/tests/utils/__init__.py
@@ -20,7 +20,7 @@ TENANT = "fb-test"
 
 def wait_for_platform() -> None:
     if TEST_MODE == TestMode.REMOTE:
-        time.sleep(60)
+        time.sleep(120)
         return
     platform_server_health_check = PLATFORM_SERVER_HEALTH_CHECK_URL
     if TEST_MODE == TestMode.LOCAL:
