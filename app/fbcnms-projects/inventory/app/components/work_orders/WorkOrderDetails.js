0ce7feb09f29f98ce0ea531b82c116e5b9161bf4
dlayzaqu <dlayzaqu@emeal.nttdata.com>
2021-11-10 12:23:51 -0500

    [feat] recover checklist mandatory

--- app/fbcnms-projects/inventory/app/components/work_orders/WorkOrderDetails.js
+++ app/fbcnms-projects/inventory/app/components/work_orders/WorkOrderDetails.js
@@ -23,7 +23,7 @@ import type {Property} from '../../common/Property';
 import type {DocumentCategoryNode} from '../../common/LocationType';
 import type {WithAlert} from '@fbcnms/ui/components/Alert/withAlert';
 import type {WorkOrderDetails_workOrder} from './__generated__/WorkOrderDetails_workOrder.graphql.js';
-
+import type {CheckListItem} from '../checklist/checkListCategory/ChecklistItemsDialogMutateState.js';
 import AddHyperlinkButton from '../AddHyperlinkButton';
 import AddHyperlinkMutation from '../../mutations/AddHyperlinkMutation';
 import AddImageMutation from '../../mutations/AddImageMutation';
@@ -164,6 +164,7 @@ const WorkOrderDetails = ({
   onWorkOrderRemoved,
   onCancelClicked,
   confirm,
+  alert,
 }: Props) => {
   const classes = useStyles();
   const [workOrder, setWorkOrder] = useState<WorkOrderDetails_workOrder>(
@@ -505,8 +506,18 @@ const WorkOrderDetails = ({
             'Verification message details',
           ),
           confirmLabel: Strings.common.okButton,
-        }).then(confirmed => {
+        }).then(async confirmed => {
           if (confirmed) {
+            const items: Array<CheckListItem> = editingCategories.flatMap(
+              x => x.checkList || [],
+            );
+            const isNotDone = await verifyMandatoryItems(items);
+            if (isNotDone) {
+              alert(
+                `There are mandatory checklist items pending to be answered. Please complete them before closing the Work Order.`,
+              );
+              return;
+            }
             resolve();
           } else {
             reject();
@@ -515,8 +526,22 @@ const WorkOrderDetails = ({
       }
     });
 
-    verification.then(() => {
-      setWorkOrder({...workOrder, status: value});
+    verification
+      .then(() => {
+        setWorkOrder({...workOrder, status: value});
+      })
+      .catch(x => console.error('error', x));
+  };
+
+  const verifyMandatoryItems = async (itemsArray: Array<CheckListItem>) => {
+    return itemsArray.some(value => {
+      const isMandatory = value.isMandatory;
+      if (!isMandatory) {
+        return false;
+      }
+      const isDone = isChecklistItemDone(value);
+
+      return !isDone;
     });
   };
 
