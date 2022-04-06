791512263509fc9d5edadaeb116abffd853ceca9
AdrianMolero <adrian_molero94@hotmail.com>
2022-04-06 12:13:47 -0500

    Revert "Updated files with development"
    This reverts commit 6ab84cc50b6d9537679c08dbbd84ce64c14b2963.

--- graph/graphql/resolver/query.go
+++ graph/graphql/resolver/query.go
@@ -327,44 +327,6 @@ func (r queryResolver) Counters(
 		)
 }
 
-func (r queryResolver) ResourceSpecificationRelationships(
-	ctx context.Context,
-	after *ent.Cursor, first *int,
-	before *ent.Cursor, last *int,
-	orderBy *ent.ResourceSpecificationRelationshipOrder,
-	filterBy []*models.ResourceSpecificationRelationshipFilterInput,
-) (*ent.ResourceSpecificationRelationshipConnection, error) {
-	return r.ClientFrom(ctx).
-		ResourceSpecificationRelationship.
-		Query().
-		Paginate(ctx, after, first, before, last,
-			ent.WithResourceSpecificationRelationshipOrder(orderBy),
-			ent.WithResourceSpecificationRelationshipFilter(
-				func(query *ent.ResourceSpecificationRelationshipQuery) (*ent.ResourceSpecificationRelationshipQuery, error) {
-					return resolverutil.ResourceSpecificationRelationshipFilter(query, filterBy)
-				},
-			),
-		)
-}
-
-func (r queryResolver) ResourceSpecificationItems(
-	ctx context.Context,
-	after *ent.Cursor, first *int,
-	before *ent.Cursor, last *int,
-	filterBy []*models.ResourceSpecificationItemsFilterInput,
-) (*ent.ResourceSpecificationItemsConnection, error) {
-	return r.ClientFrom(ctx).
-		ResourceSpecificationItems.
-		Query().
-		Paginate(ctx, after, first, before, last,
-			ent.WithResourceSpecificationItemsFilter(
-				func(query *ent.ResourceSpecificationItemsQuery) (*ent.ResourceSpecificationItemsQuery, error) {
-					return resolverutil.ResourceSpecificationItemsFilter(query, filterBy)
-				},
-			),
-		)
-}
-
 func (r queryResolver) Kpis(
 	ctx context.Context,
 	after *ent.Cursor, first *int,
@@ -652,65 +614,6 @@ func (r queryResolver) NetworkTypes(
 			ent.WithNetworkTypeFilter(
 				func(query *ent.NetworkTypeQuery) (*ent.NetworkTypeQuery, error) {
 					return resolverutil.NetworkTypeFilter(query, filterBy)
-				},
-			),
-		)
-}
-
-func (r queryResolver) ResourceTypes(
-	ctx context.Context,
-	after *ent.Cursor, first *int,
-	before *ent.Cursor, last *int,
-	orderBy *ent.ResourceTypeOrder,
-	filterBy []*models.ResourceTypeFilterInput,
-) (*ent.ResourceTypeConnection, error) {
-	return r.ClientFrom(ctx).
-		ResourceType.
-		Query().
-		Paginate(ctx, after, first, before, last,
-			ent.WithResourceTypeOrder(orderBy),
-			ent.WithResourceTypeFilter(
-				func(query *ent.ResourceTypeQuery) (*ent.ResourceTypeQuery, error) {
-					return resolverutil.ResourceTypeFilter(query, filterBy)
-				},
-			),
-		)
-}
-
-func (r queryResolver) ResourceTypeRelationships(
-	ctx context.Context,
-	after *ent.Cursor, first *int,
-	before *ent.Cursor, last *int,
-	orderBy *ent.ResourceTypeRelationshipOrder,
-	filterBy []*models.ResourceTypeRelationshipFilterInput,
-) (*ent.ResourceTypeRelationshipConnection, error) {
-	return r.ClientFrom(ctx).
-		ResourceTypeRelationship.
-		Query().
-		Paginate(ctx, after, first, before, last,
-			ent.WithResourceTypeRelationshipOrder(orderBy),
-			ent.WithResourceTypeRelationshipFilter(
-				func(query *ent.ResourceTypeRelationshipQuery) (*ent.ResourceTypeRelationshipQuery, error) {
-					return resolverutil.ResourceTypeRelationshipFilter(query, filterBy)
-				},
-			),
-		)
-}
-func (r queryResolver) ResourceSpecifications(
-	ctx context.Context,
-	after *ent.Cursor, first *int,
-	before *ent.Cursor, last *int,
-	orderBy *ent.ResourceSpecificationOrder,
-	filterBy []*models.ResourceSpecificationFilterInput,
-) (*ent.ResourceSpecificationConnection, error) {
-	return r.ClientFrom(ctx).
-		ResourceSpecification.
-		Query().
-		Paginate(ctx, after, first, before, last,
-			ent.WithResourceSpecificationOrder(orderBy),
-			ent.WithResourceSpecificationFilter(
-				func(query *ent.ResourceSpecificationQuery) (*ent.ResourceSpecificationQuery, error) {
-					return resolverutil.ResourceSpecificationFilter(query, filterBy)
 				},
 			),
 		)
