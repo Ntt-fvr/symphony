45cf9c7a2334ec4a13fe54a5f5d168d7393a12d5
Sergio Hortua <sergioandresjsv@gmail.com>
2022-04-28 15:15:26 -0500

    fix styles scroll modal SelectDateTime

--- app/fbcnms-projects/inventory/app/components/resource_instance/SelectDateTime.js
+++ app/fbcnms-projects/inventory/app/components/resource_instance/SelectDateTime.js
@@ -11,6 +11,7 @@
 import * as React from 'react';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
+import symphony from '@symphony/design-system/theme/symphony';
 import {makeStyles} from '@material-ui/styles';
 
 const useStyles = makeStyles(() => ({
@@ -22,7 +23,27 @@ const useStyles = makeStyles(() => ({
   },
   contenedor: {
     height: 300,
+    margin: '0 20px',
     overflow: 'auto',
+    paddingRight: '9px',
+    maxHeight: 'calc(95vh - 156px)',
+    '&::-webkit-scrollbar': {
+      width: '9px',
+    },
+    '&::-webkit-scrollbar-thumb': {
+      background: symphony.palette.D300,
+      borderRadius: '4px',
+    },
+    '&::-webkit-scrollbar-thumb:active': {
+      background: symphony.palette.D200,
+    },
+    '&::-webkit-scrollbar-thumb:hover': {
+      background: symphony.palette.D400,
+    },
+    '&::-webkit-scrollbar-track': {
+      background: symphony.palette.D100,
+      borderRadius: '4px',
+    },
   },
   item: {
     '&:hover': {
