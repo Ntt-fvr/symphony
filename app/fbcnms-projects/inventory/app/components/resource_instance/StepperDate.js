23fc74c0b295e35aefec7f1d0e349f015203838b
Sergio Hortua <sergioandresjsv@gmail.com>
2022-04-29 12:15:46 -0500

    Implement state initial into stepper modal rollback

--- app/fbcnms-projects/inventory/app/components/resource_instance/StepperDate.js
+++ app/fbcnms-projects/inventory/app/components/resource_instance/StepperDate.js
@@ -9,11 +9,15 @@
  */
 
 import * as React from 'react';
-import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord';
+import Button from '@material-ui/core/Button';
+// import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord';
+import LensIcon from '@material-ui/icons/Lens';
+import PanoramaFishEyeIcon from '@material-ui/icons/PanoramaFishEye';
 import Step from '@material-ui/core/Step';
 import StepConnector from '@material-ui/core/StepConnector';
 import StepLabel from '@material-ui/core/StepLabel';
 import Stepper from '@material-ui/core/Stepper';
+import classNames from 'classnames';
 import {makeStyles} from '@material-ui/styles';
 import {withStyles} from '@material-ui/core/styles';
 
@@ -29,7 +33,27 @@ const useStyles = makeStyles(() => ({
   stepLabelRoot: {
     color: 'blue',
   },
+  //*********************** */
+  rootStep: {
+    color: 'yellow',
+    display: 'flex',
+    height: 22,
+    alignItems: 'center',
+  },
+  activeStep: {
+    color: '#784af4',
+  },
+  circleStep: {
+    fontSize: '16px',
+    color: 'black',
+  },
+  completedStep: {
+    fontSize: '16px',
+    color: 'red',
+    zIndex: 1,
+  },
 }));
+
 const QontoConnector = withStyles({
   alternativeLabel: {
     top: 10,
@@ -38,20 +62,58 @@ const QontoConnector = withStyles({
   },
   active: {
     '& $line': {
-      borderColor: 'blue',
+      borderColor: 'red',
     },
   },
   completed: {
     '& $line': {
-      borderColor: '#784af4',
+      borderColor: 'green',
     },
   },
   line: {
-    borderColor: 'blue',
+    borderColor: 'black',
     borderTopWidth: 3,
     borderRadius: 1,
   },
 })(StepConnector);
+
+// const useQontoStepIconStyles = makeStyles({
+//   root: {
+//     color: 'yellow',
+//     display: 'flex',
+//     height: 22,
+//     alignItems: 'center',
+//   },
+//   active: {
+//     color: '#784af4',
+//   },
+//   circle: {
+//     fontSize: '16px',
+//     color: 'black',
+//   },
+//   completed: {
+//     fontSize: '16px',
+//     color: 'red',
+//     zIndex: 1,
+//   },
+// });
+function QontoStepIcon(props) {
+  const classes = useStyles();
+  const {active, completed} = props;
+
+  return (
+    <div
+      className={classNames(classes.rootStep, {
+        [classes.activeStep]: active,
+      })}>
+      {completed ? (
+        <LensIcon className={classes.completedStep} />
+      ) : (
+        <PanoramaFishEyeIcon className={classes.circleStep} />
+      )}
+    </div>
+  );
+}
 
 const steps = ['Select Date', 'Confirm Change'];
 
@@ -59,25 +121,46 @@ type Props = $ReadOnly<{||}>;
 
 const StepperDate = (props: Props) => {
   const {} = props;
-
   const classes = useStyles();
+  const [activeStep, setActiveStep] = React.useState(0);
+
+  const handleNext = () => {
+    setActiveStep(prevActiveStep => prevActiveStep + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep(prevActiveStep => prevActiveStep - 1);
+  };
 
   return (
     <div className={classes.root}>
       <Stepper
         className={classes.stepperRoot}
         alternativeLabel
+        activeStep={activeStep}
         connector={<QontoConnector />}>
         {steps.map(label => (
           <Step key={label}>
             <StepLabel
+              StepIconComponent={QontoStepIcon}
               className={classes.stepLabelRoot}
-              icon={<FiberManualRecordIcon />}>
+              // icon={<FiberManualRecordIcon />}
+            >
               {label}
             </StepLabel>
           </Step>
         ))}
       </Stepper>
+      <div>
+        <div>
+          <Button disabled={activeStep === 0} onClick={handleBack}>
+            Back
+          </Button>
+          <Button variant="contained" color="primary" onClick={handleNext}>
+            Fecha
+          </Button>
+        </div>
+      </div>
     </div>
   );
 };
