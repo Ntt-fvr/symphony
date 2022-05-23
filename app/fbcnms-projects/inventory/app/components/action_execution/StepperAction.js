ace0b3e95c90bf3fe56e1e693ac1cfb497cde731
Sergio Hortua <sergioandresjsv@gmail.com>
2022-05-23 16:54:42 -0500

    flow on button Create Action with styles all

--- app/fbcnms-projects/inventory/app/components/action_execution/StepperAction.js
+++ app/fbcnms-projects/inventory/app/components/action_execution/StepperAction.js
@@ -11,14 +11,21 @@
 import Button from '@material-ui/core/Button';
 import Card from '@symphony/design-system/components/Card/Card';
 import CardHeader from '@symphony/design-system/components/Card/CardHeader';
+import Checkbox from '@symphony/design-system/components/Checkbox/Checkbox';
 import ConfigureTitle from './common/ConfigureTitle';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogConfirmChange from './DialogConfirmChange';
+import FormField from '@symphony/design-system/components/FormField/FormField';
 import Grid from '@material-ui/core/Grid';
+import PowerSearchBar from '../power_search/PowerSearchBar';
 import React, {useCallback, useState} from 'react';
+import Table from '@symphony/design-system/components/Table/Table';
 import Text from '@symphony/design-system/components/Text';
+import TextField from '@material-ui/core/TextField';
 import fbt from 'fbt';
+import symphony from '@symphony/design-system/theme/symphony';
+import {MenuItem} from '@material-ui/core';
 import {SelectDateTime} from './SelectDateTime';
 import {StepToStep} from './StepToStep';
 import {makeStyles} from '@material-ui/styles';
@@ -34,19 +41,54 @@ const useStyles = makeStyles(() => ({
   subTitle: {
     color: 'blue',
   },
-  dialogActions: {
-    padding: '0 24px',
-    marginBottom: '30px',
-    bottom: 0,
-    display: 'flex',
-    justifyContent: 'flex-end',
-    width: '100%',
-    backgroundColor: 'rgba(255, 255, 255, 0.9)',
-    zIndex: 2,
+  formField: {
+    '& .MuiOutlinedInput-notchedOutline': {
+      borderColor: symphony.palette.D200,
+    },
+    '& .Mui-focused .MuiOutlinedInput-notchedOutline': {
+      borderColor: symphony.palette.B600,
+    },
+    '& .MuiInputLabel-outlined.MuiInputLabel-shrink': {
+      transform: 'translate(14px, -3px) scale(0.85)',
+    },
+    '& .MuiFormControl-root': {
+      marginBottom: '36px',
+      '&:hover .MuiOutlinedInput-notchedOutline': {
+        borderColor: symphony.palette.B600,
+      },
+    },
+    '& .MuiOutlinedInput-input': {
+      paddingTop: '7px',
+      paddingBottom: '7px',
+      fontSize: '14px',
+      display: 'flex',
+      alignItems: 'center',
+    },
+    '& label': {
+      fontSize: '14px',
+      lineHeight: '8px',
+    },
   },
   option: {
     width: '111px',
     height: '36px',
+  },
+  bar: {
+    display: 'flex',
+    flexDirection: 'row',
+    boxShadow: '0px 1px 3px 0px rgba(0, 0, 43, 0.15)',
+  },
+  searchBar: {
+    flexGrow: 1,
+  },
+  selectField: {
+    width: '200px',
+    '& .MuiSelect-selectMenu ': {
+      height: '16px',
+    },
+  },
+  tableInside: {
+    height: '280px',
   },
 }));
 
@@ -55,14 +97,79 @@ type Props = $ReadOnly<{|
   onClose: () => void,
   isDialogSelectDate: boolean,
 |}>;
-
+const tableColumns = [
+  {
+    key: 'id',
+    title: 'ID',
+    getSortingValue: row => row.id,
+    render: row => (
+      <Button color="primary" variant="text" tooltip={row.id ?? ''}>
+        {row.id}
+      </Button>
+    ),
+  },
+  {
+    key: 'location',
+    title: 'Location',
+    getSortingValue: row => row.location,
+    render: row => (
+      <Button color="primary" variant="text" tooltip={row.location ?? ''}>
+        {row.location}
+      </Button>
+    ),
+  },
+];
+const data = [
+  {
+    id: '386547056643',
+    key: '386547056643',
+    location: 'S17161',
+  },
+  {
+    id: '386547056644',
+    key: '386547056644',
+    location: 'S17162',
+  },
+  {
+    id: '386547056645',
+    key: '386547056645',
+    location: 'S17163',
+  },
+  {
+    id: '386547056646',
+    key: '386547056646',
+    location: 'S17164',
+  },
+  {
+    id: '386547056647',
+    key: '386547056647',
+    location: 'S17165',
+  },
+  {
+    id: '386547056648',
+    key: '386547056648',
+    location: 'S17166',
+  },
+  {
+    id: '386547056649',
+    key: '386547056649',
+    location: 'S17167',
+  },
+  {
+    id: '386547056650',
+    key: '386547056650',
+    location: 'S17168',
+  },
+];
 const StepperAction = (props: Props) => {
   const {onClose, isDialogSelectDate} = props;
+  const [setFilters] = useState([]);
   const [isDialogConfirmChange, setIsDialogConfirmChange] = useState(
     isDialogSelectDate,
   );
 
   const [activeStep, setActiveStep] = useState(0);
+  const [checked, setChecked] = useState(true);
   const [selectedDate, setSelectedDate] = useState(null);
 
   const handleSelectDate = useCallback(clickedDate => {
@@ -97,7 +204,82 @@ const StepperAction = (props: Props) => {
               Use the filter to find resources to apply the actions
             </Text>
           </Grid>
-          <Grid>
+          <Grid
+            style={{
+              marginBottom: '30px',
+              borderBottom: '1px solid',
+              color: symphony.palette.D100,
+            }}
+            container
+            direction="row"
+            spacing={3}>
+            <Grid item xs={5}>
+              <TextField
+                className={classes.formField}
+                required
+                label="Name"
+                fullWidth
+                name="name"
+                variant="outlined"
+                defaultValue={''}
+              />
+            </Grid>
+            <Grid item xs={5}>
+              <TextField
+                className={classes.formField}
+                required
+                label="Description"
+                fullWidth
+                name="description"
+                variant="outlined"
+                defaultValue={''}
+              />
+            </Grid>
+          </Grid>
+          <Grid item xs={12}>
+            <div className={classes.bar}>
+              <div className={classes.searchBar}>
+                <PowerSearchBar
+                  placeholder="Filter Resource Type"
+                  getSelectedFilter={filters => setFilters(filters)}
+                  onFiltersChanged={filters => setFilters(filters)}
+                  filterConfigs={[]}
+                  searchConfig={[]}
+                  entity={'SERVICE'}
+                />
+              </div>
+            </div>
+          </Grid>
+          <Grid container justify="space-between">
+            <Checkbox
+              checked={checked}
+              title="Select All"
+              onChange={selection =>
+                setChecked(selection === 'checked' ? true : false)
+              }
+            />
+            <TextField
+              required
+              id="outlined-select-family"
+              select
+              className={classes.selectField}
+              label="Sleep"
+              name="family"
+              defaultValue=""
+              variant="outlined">
+              <MenuItem>Reset</MenuItem>
+              <MenuItem>Sleep</MenuItem>
+              <MenuItem>Update</MenuItem>
+            </TextField>
+          </Grid>
+          <Grid item xs={12} style={{margin: '20px 0 0 0'}}>
+            <Table
+              className={classes.tableInside}
+              data={data}
+              columns={tableColumns}
+            />
+          </Grid>
+          <Grid container>
             <Button
               className={classes.option}
               variant="outlined"
