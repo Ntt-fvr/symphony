8d7273c231193acccc49c908d9cbcc566c47b61d
teolopera <msernalo@emeal.nttdata.com>
2022-05-24 15:13:33 -0500

    Menu topbar updated - Flow Card and new modal created - HandleMenuDialog

--- app/fbcnms-projects/inventory/app/components/automation/flows/builder/tools/MenuTopBar.js
+++ app/fbcnms-projects/inventory/app/components/automation/flows/builder/tools/MenuTopBar.js
@@ -16,11 +16,15 @@ import ListItemText from '@material-ui/core/ListItemText';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
-import React from 'react';
+import React, {useState} from 'react';
+import ReportProblemOutlinedIcon from '@material-ui/icons/ReportProblemOutlined';
+import Text from '@symphony/design-system/components/Text';
 import Tooltip from '../../../inputs/Tooltip';
+import fbt from 'fbt';
 import {BLUE, DARK} from '@symphony/design-system/theme/symphony';
 import {Divider} from '@material-ui/core';
 import {DuplicateFlowIcon, EditFlowIcon} from '@symphony/design-system/icons';
+import {HandleMenuDialog} from './HandleMenuDialog';
 import {makeStyles} from '@material-ui/styles';
 import {withStyles} from '@material-ui/core/styles';
 
@@ -54,7 +58,7 @@ const StyledMenu = withStyles({
       'linear-gradient(180deg, #FFFFFF 0%, rgba(255, 255, 255, 0) 100%), #FFFFFF',
     boxShadow: '0px 4px 10px 2px rgba(0, 0, 0, 0.15)',
     borderRadius: 4,
-    padding: '5px 0px',
+    padding: '0px 0px',
     minWidth: 172,
     marginTop: 6,
     '& .MuiList-padding': {
@@ -106,10 +110,18 @@ const StyledMenuItem = withStyles({
   },
 })(MenuItem);
 
-export default function CustomizedMenus() {
+type Props = $ReadOnly<{|
+  icon?: any,
+|}>;
+
+export default function CustomizedMenus(props: Props) {
+  const {icon} = props;
+
   const classes = useStyles();
 
   const [anchorEl, setAnchorEl] = React.useState(null);
+  const [openMenuOpt, setOpenMenuOpt] = useState(false);
+  const [activeModal, setActiveModal] = useState(0);
 
   const handleClick = event => {
     setAnchorEl(event.currentTarget);
@@ -119,13 +131,19 @@ export default function CustomizedMenus() {
     setAnchorEl(null);
   };
 
+  const openModal = modalId => {
+    setActiveModal(modalId);
+    setAnchorEl(null);
+    setOpenMenuOpt(true);
+  };
+
   return (
     <div className={classes.root}>
       <Tooltip tooltip={'More Actions'}>
         <IconButton
           skin={'inherit'}
           onClick={handleClick}
-          icon={MoreHorizIcon}
+          icon={icon ? icon : MoreHorizIcon}
         />
       </Tooltip>
       <StyledMenu
@@ -148,19 +166,56 @@ export default function CustomizedMenus() {
           <ListItemText primary="Duplicate flow" />
         </StyledMenuItem>
         <Divider />
-        <StyledMenuItem>
+        <StyledMenuItem onClick={() => openModal(1)}>
           <ListItemIcon>
             <ArchiveOutlinedIcon color={'secondary'} />
           </ListItemIcon>
           <ListItemText primary="Archive flow" />
         </StyledMenuItem>
-        <StyledMenuItem>
+        <StyledMenuItem onClick={() => openModal(2)}>
           <ListItemIcon>
             <DeleteOutlineIcon color={'secondary'} />
           </ListItemIcon>
           <ListItemText primary="Delete flow" />
         </StyledMenuItem>
       </StyledMenu>
+      {activeModal === 1 && (
+        <HandleMenuDialog
+          icon={<ArchiveOutlinedIcon />}
+          openModal={setOpenMenuOpt}
+          isOpen={openMenuOpt}
+          activeModal={activeModal}
+          text={
+            <Text variant="subtitle1">
+              <fbt desc="">
+                ¿Are you sure you want to archive the current flow?{' '}
+                <span style={{fontWeight: 'bold'}}>
+                  The flows that were started will finish running, but new flows
+                  can no longer be instantiated.
+                </span>
+              </fbt>
+            </Text>
+          }
+        />
+      )}
+      {activeModal === 2 && (
+        <HandleMenuDialog
+          icon={<ReportProblemOutlinedIcon />}
+          openModal={setOpenMenuOpt}
+          isOpen={openMenuOpt}
+          activeModal={activeModal}
+          text={
+            <Text variant="subtitle1">
+              <fbt desc="">
+                ¿Are you sure you want to delete the current flow?{' '}
+                <span style={{fontWeight: 'bold'}}>
+                  This option cannot be undone
+                </span>
+              </fbt>
+            </Text>
+          }
+        />
+      )}
     </div>
   );
 }
