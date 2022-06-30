534e0d379227e254970b54f0fe552f68a6962b33
Sergio Hortua <sergioandresjsv@gmail.com>
2022-06-30 02:37:10 -0500

    clean code fileds test

--- app/fbcnms-projects/inventory/app/components/configuration_management/__tests__/CardChangeRequestSchedule.test.js
+++ app/fbcnms-projects/inventory/app/components/configuration_management/__tests__/CardChangeRequestSchedule.test.js
@@ -19,7 +19,7 @@ describe('suite test component <CardChangeRequestSchedule/>', () => {
     weekDay: 'MONDAY',
   };
 
-  it('CM-FE-12001 render titles into card <CardChangeRequestSchedule/>', () => {
+  it('CM-04-FE-12001 render titles into card <CardChangeRequestSchedule/>', () => {
     render(<CardChangeRequestSchedule />);
     const textRadioButonOne = screen.getByText(/As soon as approved/i);
     expect(textRadioButonOne).toBeInTheDocument();
@@ -31,7 +31,7 @@ describe('suite test component <CardChangeRequestSchedule/>', () => {
     expect(textSubtitleOnCard).not.toBeInTheDocument();
   });
 
-  it('CM-FE-12002 event onClick radio button As soon as approved', () => {
+  it('CM-04-FE-12002 event onClick radio button As soon as approved', () => {
     const handleClick = jest.fn();
     render(<CardChangeRequestSchedule {...mockProps} onClick={handleClick} />);
     const textRadioButonOne = screen.getByLabelText(/As soon as approved/i);
@@ -40,7 +40,7 @@ describe('suite test component <CardChangeRequestSchedule/>', () => {
     expect(handleClick);
   });
 
-  it('CM-FE-12003 event onClick radio button Schedule with approval', () => {
+  it('CM-04-FE-12003 event onClick radio button Schedule with approval', () => {
     const handleClick = jest.fn();
     render(<CardChangeRequestSchedule {...mockProps} onClick={handleClick} />);
     const textRadioButonOne = screen.getByLabelText(/Schedule with approval/i);
@@ -49,57 +49,10 @@ describe('suite test component <CardChangeRequestSchedule/>', () => {
     expect(handleClick);
   });
 
-  it('CM-FE-12003 event onClick radio button Schedule with approval', () => {
-    const handleClick = jest.fn();
-    render(<CardChangeRequestSchedule {...mockProps} onClick={handleClick} />);
-    const textRadioButonOne = screen.getByLabelText(/Schedule with approval/i);
-    expect(textRadioButonOne).toBeInTheDocument();
-    fireEvent.click(screen.getByDisplayValue(/approval/i));
-    expect(handleClick);
-  });
-
-  it('CM-FE-12003 render on card mode schedule with approval', () => {
+  it('CM-04-FE-12004 render data from props on card mode schedule with approval', () => {
     const handleClick = jest.fn();
     render(<CardChangeRequestSchedule schedule={mockProps} />);
     fireEvent.click(screen.getByLabelText(/Schedule with approval/i));
     expect(handleClick);
   });
-
-  /*
-  it('CM-FE-12002 display props', () => {
-    render(<DialogMapping {...mockProps} />);
-    screen.getByText(/Title of Modal/i);
-  });
-
-  it('CM-FE-12003 display info from props into inputs and name CP', () => {
-    const Padre = props => {
-      return (
-        <div>
-          <DialogMapping {...props} />
-        </div>
-      );
-    };
-    render(<Padre {...mockProps} />);
-    const mappingIn = screen.getByDisplayValue(/Regular phrase In/i);
-    expect(mappingIn).toBeInTheDocument();
-    const mappingOut = screen.getByDisplayValue(/Regular phrase Out/i);
-    expect(mappingOut).toBeInTheDocument();
-    const TitleModal = screen.getByText(/name of Configuration Parameter/i);
-    expect(TitleModal).toBeInTheDocument();
-  });
-
-  it('CM-FE-12004 event onClick button Cancel', () => {
-    const handleClick = jest.fn();
-    render(<DialogMapping {...mockProps} onClick={handleClick} />);
-    fireEvent.click(screen.getByText(/Cancel/i));
-    expect(handleClick);
-  });
-
-  it('CM-FE-12005 event onClick button Save', () => {
-    const handleClick = jest.fn();
-    render(<DialogMapping {...mockProps} onClick={handleClick} />);
-    fireEvent.click(screen.getByText(/Save/i));
-    expect(handleClick);
-  });
-*/
 });
--- app/fbcnms-projects/inventory/app/components/configuration_management/__tests__/TableResource.test.js
+++ app/fbcnms-projects/inventory/app/components/configuration_management/__tests__/TableResource.test.js
@@ -26,7 +26,7 @@ describe('suite test component <TableResource/>', () => {
     ],
   };
 
-  it('CM-03-FE-12001 render titles into card <TableResource/>', () => {
+  it('CM-04-FE-12001 render titles into card <TableResource/>', () => {
     render(<TableResource />);
     const textTitleResource = screen.getByText(/Resource/i);
     expect(textTitleResource).toBeInTheDocument();
@@ -40,7 +40,7 @@ describe('suite test component <TableResource/>', () => {
     expect(textInexistent).not.toBeInTheDocument();
   });
 
-  it('CM-FE-12003 display info from props into fields of table', () => {
+  it('CM-04-FE-12002 display info from props into fields of table', () => {
     render(<TableResource valuesTable={mockValuesTable} />);
     const resourceName = screen.getAllByText(/Resource Test/i);
     expect(resourceName).toBeInTheDOM;
@@ -50,76 +50,5 @@ describe('suite test component <TableResource/>', () => {
     expect(currentValue).toBeInTheDOM;
     const newValue = screen.getAllByText(/New/i);
     expect(newValue).toBeInTheDOM;
-
-    screen.debug(resourceName);
   });
-
-  /*
-  it('CM-FE-12002 event onClick radio button As soon as approved', () => {
-    const handleClick = jest.fn();
-    render(<CardChangeRequestSchedule {...mockProps} onClick={handleClick} />);
-    const textRadioButonOne = screen.getByLabelText(/As soon as approved/i);
-    expect(textRadioButonOne).toBeInTheDocument();
-    fireEvent.click(screen.getByDisplayValue(/approved/i));
-    expect(handleClick);
-  });
-
-  it('CM-FE-12003 event onClick radio button Schedule with approval', () => {
-    const handleClick = jest.fn();
-    render(<CardChangeRequestSchedule {...mockProps} onClick={handleClick} />);
-    const textRadioButonOne = screen.getByLabelText(/Schedule with approval/i);
-    expect(textRadioButonOne).toBeInTheDocument();
-    fireEvent.click(screen.getByDisplayValue(/approval/i));
-    expect(handleClick);
-    screen.debug();
-  });
-
-  it('CM-FE-12003 event onClick radio button Schedule with approval', () => {
-    const handleClick = jest.fn();
-    render(<CardChangeRequestSchedule {...mockProps} onClick={handleClick} />);
-    const textRadioButonOne = screen.getByLabelText(/Schedule with approval/i);
-    expect(textRadioButonOne).toBeInTheDocument();
-    fireEvent.click(screen.getByDisplayValue(/approval/i));
-    expect(handleClick);
-    screen.debug(screen.getByDisplayValue(/approval/i));
-  });
-  */
-
-  /*
-  it('CM-FE-12002 display props', () => {
-    render(<DialogMapping {...mockProps} />);
-    screen.getByText(/Title of Modal/i);
-  });
-
-  it('CM-FE-12003 display info from props into inputs and name CP', () => {
-    const Padre = props => {
-      return (
-        <div>
-          <DialogMapping {...props} />
-        </div>
-      );
-    };
-    render(<Padre {...mockProps} />);
-    const mappingIn = screen.getByDisplayValue(/Regular phrase In/i);
-    expect(mappingIn).toBeInTheDocument();
-    const mappingOut = screen.getByDisplayValue(/Regular phrase Out/i);
-    expect(mappingOut).toBeInTheDocument();
-    const TitleModal = screen.getByText(/name of Configuration Parameter/i);
-    expect(TitleModal).toBeInTheDocument();
-  });
-
-  it('CM-FE-12004 event onClick button Cancel', () => {
-    const handleClick = jest.fn();
-    render(<DialogMapping {...mockProps} onClick={handleClick} />);
-    fireEvent.click(screen.getByText(/Cancel/i));
-    expect(handleClick);
-  });
-
-  it('CM-FE-12005 event onClick button Save', () => {
-    const handleClick = jest.fn();
-    render(<DialogMapping {...mockProps} onClick={handleClick} />);
-    fireEvent.click(screen.getByText(/Save/i));
-    expect(handleClick);
-  });
-*/
 });
