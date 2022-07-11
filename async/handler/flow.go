daea9bde80d61fd1cd2a06119279603b770ea2d6
mescorci <mescorci@everis.com>
2022-07-11 09:57:38 -0500

    update supergraph add kafka block

--- async/handler/flow.go
+++ async/handler/flow.go
@@ -6,6 +6,7 @@ package handler
 
 import (
 	"context"
+	"fmt"
 	"time"
 
 	"github.com/facebookincubator/symphony/async/worker"
@@ -31,13 +32,17 @@ func NewFlowHandler(client client.Client) *FlowHandler {
 
 // Handle handles event based on relevant logic
 func (f FlowHandler) Handle(ctx context.Context, _ log.Logger, evt ev.EventObject) error {
+	fmt.Println("FlowHandler")
 	entry, ok := evt.(event.LogEntry)
+	fmt.Println("evt.(event.LogEntry)", entry, ok)
 	if !ok || entry.Type != ent.TypeFlowInstance || !entry.Operation.Is(ent.OpCreate) {
+		fmt.Println("no es de ent.TypeFlowInstance")
 		return nil
 	}
 
 	v := viewer.FromContext(ctx)
 	if !v.Features().Enabled(viewer.FeatureExecuteAutomationFlows) {
+		fmt.Println("FeatureExecuteAutomationFlows disabled")
 		return nil
 	}
 
@@ -50,7 +55,7 @@ func (f FlowHandler) Handle(ctx context.Context, _ log.Logger, evt ev.EventObjec
 	runFlowInput := worker.RunFlowInput{
 		FlowInstanceID: entry.CurrState.ID,
 	}
-
+	fmt.Println("FlowInstanceID ", entry.CurrState.ID)
 	_, err := f.client.StartWorkflow(
 		ctx, workflowOptions, worker.RunFlowWorkflowName,
 		ctx, worker.TaskListName, runFlowInput,
