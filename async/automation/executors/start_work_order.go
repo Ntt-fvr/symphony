8c25c02392c9f46ef6d3751981e12258d6351837
Felix Diaz Galvan <felixjr.developer@gmail.com>
2022-07-14 23:06:48 -0500

    add creation workorder for block

--- async/automation/executors/start_work_order.go
+++ async/automation/executors/start_work_order.go
@@ -2,9 +2,16 @@ package executors
 
 import (
 	"context"
+	"fmt"
+	"github.com/AlekSi/pointer"
+	"github.com/facebookincubator/symphony/graph/graphql/models"
+	"github.com/facebookincubator/symphony/graph/resolverutil"
 	"github.com/facebookincubator/symphony/pkg/ent"
+	"github.com/facebookincubator/symphony/pkg/ent/propertytype"
 	"github.com/facebookincubator/symphony/pkg/ent/workorder"
 	"github.com/facebookincubator/symphony/pkg/viewer"
+	"github.com/pkg/errors"
+	"github.com/vektah/gqlparser/v2/gqlerror"
 )
 
 type ExecutorStartWorkOrderBlock struct {
@@ -12,9 +19,9 @@ type ExecutorStartWorkOrderBlock struct {
 }
 
 func (b *ExecutorStartWorkOrderBlock) runLogic() error {
-	ctx := context.Context(nil)
+	ctx := context.Background()
 	input := b.input
-	mutation := ent.FromContext(nil).
+	mutation := ent.FromContext(ctx).
 		WorkOrder.Create().
 		SetName(input["Name"].(string)).
 		SetTemplateID(input["workOrderTypeId"].(int))
@@ -31,44 +38,112 @@ func (b *ExecutorStartWorkOrderBlock) runLogic() error {
 	} else {
 		v, ok := viewer.FromContext(ctx).(*viewer.UserViewer)
 		if !ok {
-			return nil // gqlerror.Errorf("could not be executed in automation")
+			return gqlerror.Errorf("could not be executed in automation")
 		}
 		mutation = mutation.SetOwner(v.User())
 	}
-	/*
-		wo, err := mutation.Save(ctx)
-		if err != nil {
-			return nil //errors.Wrap(err, "creating work order")
-		}
-		tmpl, err := wo.QueryTemplate().Only(ctx)
-		if err != nil {
-			return nil // err
-		}
-		tPropInputs, err := r.convertToTemplatePropertyInputs(ctx, tmpl, input.Properties)
-		if err != nil {
-			return nil, fmt.Errorf("convert to template property inputs: %w", err)
-		}
-		skipMandatoryPropertiesCheck = skipMandatoryPropertiesCheck || viewer.FromContext(ctx).Features().Enabled(viewer.FeatureMandatoryPropertiesOnWorkOrderClose)
-		propInput, err := r.validatedPropertyInputsFromTemplate(ctx, tPropInputs, tmpl.ID, enum.PropertyEntityWorkOrder, skipMandatoryPropertiesCheck)
-		if err != nil {
-			return nil, fmt.Errorf("validating property for template : %w", err)
-		}
 
-		for _, clInput := range input.CheckListCategories {
-			_, err := r.createOrUpdateCheckListCategory(ctx, clInput, wo.ID)
-			if err != nil {
-				return nil, errors.Wrap(err, "creating check list category")
-			}
-		}
-		if _, err := r.AddProperties(propInput,
-			resolverutil.AddPropertyArgs{
-				Context:    ctx,
-				EntSetter:  func(b *ent.PropertyCreate) { b.SetWorkOrderID(wo.ID) },
-				IsTemplate: pointer.ToBool(true),
-			},
-		); err != nil {
-			return nil, errors.Wrap(err, "creating work order properties")
-		}
-	*/
+	wo, err := mutation.Save(ctx)
+	if err != nil {
+		return errors.Wrap(err, "creating work order")
+	}
+	tmpl, err := wo.QueryTemplate().Only(ctx)
+	if err != nil {
+		return err
+	}
+	tPropInputs, err := convertToTemplatePropertyInputs(ctx, tmpl, input["woProperties"].([]*models.PropertyInput))
+	if err != nil {
+		return fmt.Errorf("convert to template property inputs: %w", err)
+	}
+	if _, err := AddProperties(tPropInputs,
+		resolverutil.AddPropertyArgs{
+			Context:    ctx,
+			EntSetter:  func(b *ent.PropertyCreate) { b.SetWorkOrderID(wo.ID) },
+			IsTemplate: pointer.ToBool(true),
+		},
+	); err != nil {
+		return errors.Wrap(err, "creating work order properties")
+	}
+	b.input["wo_name"] = wo.Name
+	b.input["wo_id"] = wo.ID
+	b.input["wo_assignee"], err = wo.Assignee(ctx)
+	b.output = b.input
+
 	return nil
+}
+
+func convertToTemplatePropertyInputs(
+	ctx context.Context,
+	template *ent.WorkOrderTemplate,
+	properties []*models.PropertyInput,
+) ([]*models.PropertyInput, error) {
+	client := ent.FromContext(ctx).PropertyType
+	inputs := make([]*models.PropertyInput, 0, len(properties))
+	for _, p := range properties {
+		name, err := client.Query().
+			Where(propertytype.ID(p.PropertyTypeID)).
+			Select(propertytype.FieldName).
+			String(ctx)
+		if err != nil {
+			return nil, fmt.Errorf("cannot query property type name from id: %w", err)
+		}
+		id, err := template.
+			QueryPropertyTypes().
+			Where(propertytype.Name(name)).
+			OnlyID(ctx)
+		if err != nil {
+			return nil, err
+		}
+		input := *p
+		input.PropertyTypeID = id
+		inputs = append(inputs, &input)
+	}
+	return inputs, nil
+}
+
+func AddProperties(inputs []*models.PropertyInput, args resolverutil.AddPropertyArgs) ([]*ent.Property, error) {
+	builders := make([]*ent.PropertyCreate, 0, len(inputs))
+	for _, input := range inputs {
+		builder, err := AddProperty(input, args)
+		if err != nil {
+			return nil, err
+		}
+		if builder != nil {
+			builders = append(builders, builder)
+		}
+	}
+	properties, err := ent.FromContext(args).Property.CreateBulk(builders...).Save(args)
+
+	return properties, err
+}
+
+func AddProperty(
+	input *models.PropertyInput,
+	args resolverutil.AddPropertyArgs,
+) (*ent.PropertyCreate, error) {
+	client := ent.FromContext(args)
+	propType, err := client.PropertyType.Get(args, input.PropertyTypeID)
+	if err != nil {
+		return nil, err
+	}
+	isTemplate := args.IsTemplate != nil && *args.IsTemplate
+	if !isTemplate && !propType.IsInstanceProperty {
+		return nil, nil
+	}
+	builder := client.Property.Create()
+	if args.EntSetter != nil {
+		args.EntSetter(builder)
+	}
+	builder = builder.
+		SetTypeID(input.PropertyTypeID).
+		SetNillableStringVal(input.StringValue).
+		SetNillableIntVal(input.IntValue).
+		SetNillableBoolVal(input.BooleanValue).
+		SetNillableFloatVal(input.FloatValue).
+		SetNillableLatitudeVal(input.LatitudeValue).
+		SetNillableLongitudeVal(input.LongitudeValue).
+		SetNillableRangeFromVal(input.RangeFromValue).
+		SetNillableRangeToVal(input.RangeToValue).
+		SetNillablePropertyTypeValueID(input.PropertyTypeValueID)
+	return builder, nil
 }
