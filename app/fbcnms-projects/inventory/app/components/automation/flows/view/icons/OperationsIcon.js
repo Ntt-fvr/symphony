810466bab055398371046a55d7af200abd1a1b67
Ubuntu <ubuntu@ip-10-0-12-152.ap-south-1.compute.internal>
2022-07-20 15:22:58 +0000

    Babu >> bug fix

---
+++ app/fbcnms-projects/inventory/app/components/automation/flows/view/icons/OperationsIcon.js
@@ -0,0 +1,23 @@
+/**
+ * Copyright 2004-present Facebook. All Rights Reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @flow strict-local
+ * @format
+ */
+
+ import React from 'react';
+
+ const OperationsIcon = () => (
+   <svg width="30px" height="30px" viewBox="0 0 30 30" version="1.1">
+     <path
+       fill="#FFFFFF"
+       d="M14 2H4c-1.11 0-2 .9-2 2v10h2V4h10V2zm4 4H8c-1.11 0-2 .9-2 2v10h2V8h10V6zm2 4h-8c-1.11 0-2 .9-2 2v8c0 1.1.89 2 2 2h8c1.1 0 2-.9 2-2v-8c0-1.1-.9-2-2-2z"
+     />
+   </svg>
+ );
+ 
+ export default OperationsIcon;
+ 
