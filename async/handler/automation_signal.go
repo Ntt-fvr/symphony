83f8f163af71e185c2a2e4828db34ff57ffe3589
Felix Diaz Galvan <felixjr.developer@gmail.com>
2022-07-31 19:06:59 -0500

    Create emitter for automation

--- async/handler/automation_signal.go
+++ async/handler/automation_signal.go
@@ -1,12 +1,20 @@
 package handler
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
 	"github.com/facebookincubator/symphony/pkg/ent/block"
 	"github.com/facebookincubator/symphony/pkg/ev"
 	"github.com/facebookincubator/symphony/pkg/event"
 	"github.com/facebookincubator/symphony/pkg/log"
+	"net/http"
 )
+
+type SignalRequest struct {
+	FlowInstanceId int         `json:"workflowID" binding:"required"`
+	Input          interface{} `json:"input,omitempty"`
+}
 
 func HandleAutomationSignal(ctx context.Context, _ log.Logger, evt ev.EventObject) error {
 	var err error
@@ -14,7 +22,30 @@ func HandleAutomationSignal(ctx context.Context, _ log.Logger, evt ev.EventObjec
 	if !ok || entry.Type != block.SignalTypeWOUPDATED {
 		return nil
 	}
-	//TODO:
+	var flowInstanceId int
+	if entry.FlowInstanceId != nil && *entry.FlowInstanceId > 0 {
+		flowInstanceId = *entry.FlowInstanceId
+	} else {
+		//ent.FromContext(ctx).FlowInstance.Query().
+	}
+	url := "http://automation/api/flow/1.0/signal"
+	bodyObject := &SignalRequest{
+		FlowInstanceId: flowInstanceId,
+		Input:          entry.Payload,
+	}
+
+	body, _ := json.Marshal(bodyObject)
+
+	request, err := http.NewRequest(http.MethodPost, url, bytes.NewBuffer(body))
+	if err != nil {
+		return err
+	}
+	automationClient := &http.Client{}
+	_, err = automationClient.Do(request)
+	if err != nil {
+		return err
+	}
+
 	if err != nil {
 		return err
 	}
